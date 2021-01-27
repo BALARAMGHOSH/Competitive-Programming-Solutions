831363bb4d4054c39e0d8faf2a4ab831e126549c
Balaram Ghosh <balaramghosh.net@gmail.com>
2021-01-27 21:15:24 +0530

    Create nextPermutation.java

---
+++ GFG/nextPermutation.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String a[] = in.readLine().trim().split("\\s+");
+            int arr[] = new int[N];
+            for(int i = 0;i < N;i++)
+                arr[i] = Integer.parseInt(a[i]);
+            
+            Solution ob = new Solution();
+            List<Integer> ans = new ArrayList<Integer>();
+            ans = ob.nextPermutation(N, arr);
+            StringBuilder out = new StringBuilder();
+            for(int i = 0;i < N;i++)
+                out.append(ans.get(i) + " ");
+            System.out.println(out);
+        }
+    }
+}// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+    
+    static List<Integer> nextPermutation(int n,int[] nums) {
+        int i = n - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = n - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+        
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        for(int k=0;k<n;k++){
+            list.add(nums[k]);
+        }
+        
+        return list;
+    }
+
+    private static void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
