992de5605df74258766ea4cf427712c5e853ad92
Balaram Ghosh <balaramghosh.net@gmail.com>
2021-01-27 22:40:10 +0530

    Add files via upload

---
+++ codeforces/A697.java
@@ -0,0 +1,164 @@
+/* Code by Balaram Ghosh */
+ 
+import java.io.DataInputStream; 
+import java.io.FileInputStream; 
+import java.io.IOException; 
+import java.io.InputStreamReader; 
+import java.util.Scanner; 
+import java.util.StringTokenizer; 
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class A697 
+{ 
+
+   public static void main(String[] args) throws IOException {
+        Reader s=new Reader(); 
+        int t = s.nextInt(); 
+        while(t-->0){
+            long n=s.nextLong();
+            while(n%2==0){
+                n=n/2;
+            }
+            if(n==1) System.out.println("NO");
+            else System.out.println("YES");
+        }
+    } 
+
+    // static boolean isPrime(long n) {
+    //     if(n < 2) return false;
+    //     if(n == 2 ) return false;
+    //     if( n == 3) return true;
+    //     if(n%2 == 0 || n%3 == 0) return false;
+    //     long sqrtN = (long)Math.sqrt(n)+1;
+    //     for(long i = 6L; i <= sqrtN; i += 6) {
+    //         if(n%(i-1) == 0 || n%(i+1) == 0) return false;
+    //     }
+    //     return true;
+    // }
+
+    static class Reader 
+    { 
+        final private int BUFFER_SIZE = 1 << 16; 
+        private DataInputStream din; 
+        private byte[] buffer; 
+        private int bufferPointer, bytesRead; 
+
+        public Reader() 
+        { 
+            din = new DataInputStream(System.in); 
+            buffer = new byte[BUFFER_SIZE]; 
+            bufferPointer = bytesRead = 0; 
+        } 
+
+        public Reader(String file_name) throws IOException 
+        { 
+            din = new DataInputStream(new FileInputStream(file_name)); 
+            buffer = new byte[BUFFER_SIZE]; 
+            bufferPointer = bytesRead = 0; 
+        } 
+
+        public String readLine() throws IOException 
+        { 
+            byte[] buf = new byte[64]; // line length 
+            int cnt = 0, c; 
+            while ((c = read()) != -1) 
+            { 
+                if (c == '\n') 
+                    break; 
+                buf[cnt++] = (byte) c; 
+            } 
+            return new String(buf, 0, cnt); 
+        } 
+
+        public int nextInt() throws IOException 
+        { 
+            int ret = 0; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+            do
+            { 
+                ret = ret * 10 + c - '0'; 
+            } while ((c = read()) >= '0' && c <= '9'); 
+
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+
+        public long nextLong() throws IOException 
+        { 
+            long ret = 0; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9'); 
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+
+        public double nextDouble() throws IOException 
+        { 
+            double ret = 0, div = 1; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9'); 
+
+            if (c == '.') 
+            { 
+                while ((c = read()) >= '0' && c <= '9') 
+                { 
+                    ret += (c - '0') / (div *= 10); 
+                } 
+            } 
+
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+
+        private void fillBuffer() throws IOException 
+        { 
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE); 
+            if (bytesRead == -1) 
+                buffer[0] = -1; 
+        } 
+
+        private byte read() throws IOException 
+        { 
+            if (bufferPointer == bytesRead) 
+                fillBuffer(); 
+            return buffer[bufferPointer++]; 
+        } 
+
+        public void close() throws IOException 
+        { 
+            if (din == null) 
+                return; 
+            din.close(); 
+        } 
+    } 
+} 
+
+/* Code by Balaram Ghosh */
+
