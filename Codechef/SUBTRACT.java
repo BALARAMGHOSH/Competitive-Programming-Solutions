f4730bbb351014fa9a1a0f8813a92e85a9f09b3f
Balaram Ghosh <balaramghosh.net@gmail.com>
2021-01-29 23:17:44 +0530

    Create SUBTRACT.java

---
+++ Codechef/SUBTRACT.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class SUBTRACT {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        int k=scanner.nextInt();
+        for (int i=0; i<k; i++){
+            if (n%10!=0) {
+                n=n-1;
+            }else{
+                n=n/10;
+            }
+        }       
+        System.out.println(n);
+    }
+}
